229fb658ebfadddeaeba3bfc07ba720ca185b94b
peter michael <enitanpeters28@gmail.com>
2024-01-15 13:07:01 +0100

    jwt setup

--- server/api/auth/login.post.js
+++ server/api/auth/login.post.js
@@ -1,17 +1,63 @@
 import { sendError } from "h3"
+import { getUserByUsername } from "~/server/db/users";
+import bcrypt from "bcrypt"
+import { generateTokens, sendRefreshToken } from "~/server/utils/jwt";
+import { userTransformer } from "~/server/transformers/user";
+import { createRefreshToken } from "~/server/db/refreshTokens";
+
 
 export default defineEventHandler( async ( event ) => {
 
     const body = await useBody(event)
 
-    const { username, email, password, repeatPassword, name } = body
+    const { username, password } = body
 
-    if (!username || !email || !password || !repeatPassword || !name) {
-        return sendError(event, createError({ statusCode: 400, statusMessage: 'Invalid params' }))
+
+
+    // if there is no username or password
+    if (!username || !password) {
+        return sendError(event, createError({
+            statusCode: 400,
+            statusMessage: 'Invalid Username or password'
+        }))
     }
 
-    if (password !== repeatPassword) {
-        return sendError(event, createError({ statusCode: 400, statusMessage: 'Passwords do not match' }))
+
+    // get the user from the db
+    const user = await getUserByUsername(username)
+
+
+    // if no user was found
+    if (!user) {
+        return sendError(event, createError({
+            statusCode: 400,
+            statusMessage: 'Username or password is invalid'
+        }))
+    }
+
+    // check if the password entered matches what we have in the db
+    const doesThePasswordMatch = await bcrypt.compare(password, user.password)
+
+
+    // if password doesnt match
+    if (!doesThePasswordMatch) {
+        return sendError(event, createError({
+            statusCode: 400,
+            statusMessage: 'Username or password is invalid'
+        }))
+    }
+
+    const { accessToken, refreshToken } = generateTokens(user)
+
+    await createRefreshToken({
+        token: refreshToken,
+        userId: user.id
+    })
+
+    sendRefreshToken(event, refreshToken)
+
+    return {
+        access_token: accessToken, user: userTransformer(user)
     }
 
 } );
--- server/api/auth/register.post.js
+++ server/api/auth/register.post.js
@@ -1,4 +1,6 @@
 import { sendError } from "h3"
+import { createUser } from "~/server/db/users";
+import { userTransformer } from "~/server/transformers/user";
 
 export default defineEventHandler( async ( event ) => {
 
@@ -6,10 +8,14 @@ export default defineEventHandler( async ( event ) => {
 
     const { username, email, password, repeatPassword, name } = body
 
+
+    // if any of the fields is empty
     if (!username || !email || !password || !repeatPassword || !name) {
-        return sendError(event, createError({ statusCode: 400, statusMessage: 'Invalid params' }))
+        return sendError(event, createError({ statusCode: 400, statusMessage: 'Invalid!, All fields are required' }))
     }
 
+
+    // if password and confirm password doesnt match
     if (password !== repeatPassword) {
         return sendError(event, createError({ statusCode: 400, statusMessage: 'Passwords do not match' }))
     }
@@ -23,6 +29,8 @@ export default defineEventHandler( async ( event ) => {
         profileImage: 'https://picsum.photos/200/200'
     }
 
+
+    // create a user in the db
     const user = await createUser(userData)
 
     return {
