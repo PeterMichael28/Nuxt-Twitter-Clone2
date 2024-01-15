da44ea2e3b499394ad7f49daf7350342f51bf8ed
peter michael <enitanpeters28@gmail.com>
2024-01-15 09:53:11 +0100

    project setup

---
+++ app.vue
@@ -0,0 +1,38 @@
+<template>
+  <div :class="{'dark': darkMoke}">
+
+    <div class="bg-white dark:bg-dim-900">
+
+    
+   <div class="min-h-full">
+
+    <!-- layouts -->
+    <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+      
+
+      <!-- left sidebar -->
+      <div class=" hidden md:block xs:col-span-1 lg:col-span-2">
+        <div class="sticky top-0">
+
+          <SidebarLeft />
+        </div>
+      </div>
+
+
+
+      <!-- content -->
+
+
+      <!-- right sidebar -->
+    </div>
+
+   </div>
+
+  </div>
+  </div>
+</template>
+
+<script setup>
+
+const darkMoke = ref(false)
+</script>
